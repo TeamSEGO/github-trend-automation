1d22bee69fddd1b094eacf8d170e324ca8b01957
kt.do <kt.do@samsung.com>
2015-03-11 13:59:13 +0900

    add for gitnsam

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # github-trend-automation
 1. 깃든샘으로 포팅
-2. 깃헙페이지 print
+2. 깃헙페이지 publish
+3. 그 외 ㅈㄴ 귀찮은 일들은 여기에
 
 
 * git proxy 땜에 sys.exec 로 변경
--- gitnsam.js
+++ gitnsam.js
@@ -1,32 +1,72 @@
 var fs = require("fs");
-var clone = require("nodegit").Clone.clone;
-var exec = require('child_process').exec;
-var concat = require('concat-files');
+var exec = require("child_process").exec;
+var async = require("async");
+var ProgressBar = require('progress');
 var url = "https://github.com/TeamSEGO/github-trend-kr.git";
-var tempDirName = "tmp";
-var tempPath = __dirname + "/" + tempDirName;
-var targetDir;
+var tempGHDirName = "github_tmp";
+var tempPath = __dirname + "/" + tempGHDirName;
+var weeklyName;
+var weeklyReadmeGHPath;
+var newLine = "\n\r\n\r";
+var github_cloneBar = new ProgressBar('clone[:bar]', { total: 50 });
 
 var deleteDir = function(callback){
-  exec( "rm -rf " + tempDirName, callback );
+  exec( "rm -rf " + tempGHDirName, callback );
 }
-
 var clone = function(callback){
-  exec('git clone '+ url +' '+ tempDirName, callback );
+  var timer = setInterval(function () {
+    github_cloneBar.tick();
+    if (github_cloneBar.complete) {
+      console.log('\ncomplete\n');
+      clearInterval(timer);
+    }
+  }, 100)
+  console.log("asf");
+  exec("git clone "+ url +" "+ tempGHDirName, callback );
 }
-var main = function(targetDir){
+var cloneFromGithub = function( weeklyName, callback ){
   deleteDir(clone(function(){
-    fs.readdir(tempPath + "/" + targetDir, function(err,files){
+    fs.readdir(tempPath + "/" + weeklyName, function(err,files){
       if(err)console.log(err);
-      console.log( files );
-      concat(files,'sumReadme.md', function(){
-        console.log("done");
+      weeklyReadmeGHPath = weeklyName + "-README.md";
+      fs.readFile( tempPath + "/" + weeklyName + "/README.md" , function (err, data) {
+        fs.writeFileSync( weeklyReadmeGHPath , data );
+        fs.appendFileSync( weeklyReadmeGHPath, newLine );
+        async.eachSeries(files, function(file, cb){
+          if(file=="README.md"){
+            cb();
+          }else{
+            fs.readFile( tempPath + "/" + weeklyName + "/" +file , function (err, data) {
+              if (err) throw err;
+              fs.appendFileSync(weeklyReadmeGHPath, data );
+              fs.appendFileSync(weeklyReadmeGHPath, newLine );
+              cb();
+            });
+          }
+        }. callback );
       });
     });
   }));
 }
-if(process.argv[2] == null ){
-  console.log("parameter 를 넣어주세요");
+
+var pushToGitnsam = function( callback ){
+
+}
+var sendMessageToCellWe = function(){
+
+}
+var main = function(weeklyName){
+  var callback = function(){ console.log("done"); }
+  cloneFromGithub( weeklyName, function(){
+    pushToGitnsam( function(){
+
+    })
+  } );
+}
+
+if( process.argv[2] == null ){
+  console.log("parameter 를 빠트리셨네요. \n\r"
+              +"usage : node gitnsam github_dir gitnsam_message");
 }else{
   main(process.argv[2]);
 }
